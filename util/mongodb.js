0e0853e5077765f56fee29b65c6ddd87b14b1486
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2022-12-17 18:53:19 +0330

    add env

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://localhost:27017/shop"
 
 if (!MONGODB_URI) {
   throw new Error("Your mongodb uri is not defined!");
